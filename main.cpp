f895516cb37b7e401a95b78f360ccf2cb04e55e9
小蓝 <2048327906@qq.com>
2026-06-11 23:25:37 +0800

    添加第一个C++程序

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/*Don't cut corners, don't over-rely on AI. Stay grounded, and keep challenging yourself*/
+#include <bits/stdc++.h>
+//呜呜呜呜呜呜~~~，玉了，玉了，玉了， qwq
+//输入报错可能是数组边界问题，看下循环条件关于索引的位置有没有放反
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+
+constexpr i64 INF = 998244353;
+constexpr int N = 300005;
+constexpr double IP = 2.7182818;
+constexpr i64 MAXN = 1E6 + 7;
+constexpr i64 MOD = 1E9 + 7;
+
+template<class T>
+void solve() {
+    cout << "Hello, Git and Github!" << "\n";
+    cout << "我是Dragonily, 这是我的第一个Git项目。" << "\n";
+    return ;
+}
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        solve<int>();
+    }
+
+    return 0;
+}
